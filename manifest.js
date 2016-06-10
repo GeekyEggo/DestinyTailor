c4b6e9dc630722de332e45dca9e5f1e038d57991
GeekyEggo <richard.herman@gmail.com>
2016-06-10 16:27:44 +0100

    Updated comments and documentation.

--- manifest.js
+++ manifest.js
@@ -1,99 +1,122 @@
-// Downloads DB dump from bungie manifest and creates our definition files
-// Taken and modified from kyleshays DIM repo
-// https://github.com/kyleshay/DIM/blob/master/build/processBungieManifest.js
+/**
+ * Downloads a db dump from the Bungie manifest, creating a JSON file containing only what we need
+ * {@link https://github.com/kyleshay/DIM/blob/master/build/processBungieManifest.js}.
+ */
 
-var http = require('http');
-var fs = require('fs');
-var request = require('request');
-var sqlite3 = require('sqlite3').verbose();
-var unzip = require('unzip');
+var fs = require('fs'),
+    http = require('http'),
+    request = require('request'),
+    sqlite3 = require('sqlite3').verbose(),
+    unzip = require('unzip');
 
-function writeDefinitionFile(path, name, data) {
-  var stream = fs.createWriteStream(path);
-  stream.write('var ' + name + ' = ');
-  stream.write(JSON.stringify(data, null, 2));
-  stream.write(';');
-  stream.end();
-}
-
-function onManifestRequest(error, response, body) {
-  var parsedResponse = JSON.parse(body);
-  var version = parsedResponse.Response.version;
-  var language = 'en';
-  
-  console.log('Downloading zip for ' + language + '.');
-  var zip = 'manifest/' + language + '/manifest.zip';
-  (function (zip, language) {
-    request
-      .get('https://www.bungie.net' + parsedResponse.Response.mobileWorldContentPaths[language])
-      .pipe(fs.createWriteStream(zip))
-      .on('close', function () {
-        onManifestDownloaded(zip, language);
-      });
-  })(zip, language);
-}
-
-function onManifestDownloaded(zip, language) {
-  console.log('Processing zip for ' + language);
-  fs.createReadStream(zip)
-    .pipe(unzip.Parse())
-    .on('entry', function (entry) {
-      var path = 'manifest/' + language + '/' + entry.path;
-      ws = fs.createWriteStream(path)
-      ws.on('finish', function () {
-        if (fs.existsSync(path)) {
-          extractDB(path, language);
-        }
-      });
-      entry.pipe(ws);
-    });
-}
-
-function extractDB(dbFile, language) {
-  db = new sqlite3.Database(dbFile);
-
-  db.all('SELECT * FROM DestinyInventoryItemDefinition', function(err, rows) {
-    if (err) throw err;
-
-    var DestinyArmorDefinition = {};
-
-    rows.forEach(function(row) {
-      var item = JSON.parse(row.json);
-
-      // Armor
-      if ((item.itemType === 2) || (item.itemTypeName === 'Mask')) {
-        DestinyArmorDefinition[item.itemHash] = {};
-        DestinyArmorDefinition[item.itemHash].itemName = item.itemName;
-        DestinyArmorDefinition[item.itemHash].itemTypeName = item.itemTypeName;
-        DestinyArmorDefinition[item.itemHash].itemDescription = item.itemDescription;
-        DestinyArmorDefinition[item.itemHash].icon = item.icon;
-        DestinyArmorDefinition[item.itemHash].tierType = item.tierType;
-        DestinyArmorDefinition[item.itemHash].tierTypeName = item.tierTypeName;
-      }
-
-    });
-
-    writeDefinitionFile('client/js/shared/DestinyArmorDefinition.js',    'DestinyArmorDefinition',    DestinyArmorDefinition);
-  });
-
-  db.all('SELECT * FROM DestinyStatDefinition', function(err, rows) {
-    if (err) throw err;
-
-    var DestinyStatDefinition = {};
-
-    rows.forEach(function(row) {
-      var item = JSON.parse(row.json);
-      if (item.statHash && item.statName) {
-        DestinyStatDefinition[item.statHash] = {};
-        DestinyStatDefinition[item.statHash].statName = item.statName;
-      }
-    });
-
-    writeDefinitionFile('client/js/shared/DestinyStatDefinition.js',    'DestinyStatDefinition',    DestinyStatDefinition);
-  });
-}
-
+// load the manifest from Bungie
 request.get({
-  url: 'https://www.bungie.net/Platform/Destiny/Manifest/',
-  headers: {'X-API-Key':'10E792629C2A47E19356B8A79EEFA640'}
-}, onManifestRequest);
+    url: 'https://www.bungie.net/Platform/Destiny/Manifest/',
+    headers: { 'X-API-Key': '10E792629C2A47E19356B8A79EEFA640' }
+}, onManifestResponse);
+
+/**
+ * Writes a definition file, serializing the data to a JSON string.
+ * @param {String} path The path of the file to write to.
+ * @param {String} name The variable name.
+ * @param {Object} data Object containing the definitions to serialize.
+ */
+function writeDefinitionFile(path, name, data) {
+    var contents = 'var ' + name + ' = ' + JSON.stringify(data, null, 4) + ';',
+    stream = fs.createWriteStream(path);
+
+    stream.write(contents);
+    stream.end();
+}
+
+/**
+ * Handles the response from requesting the manifest from Bungie.
+ * @param {Object} error The error.
+ * @param {Object} response The response from the request.
+ * @param {Object} body The body of the request.
+ */
+function onManifestResponse(error, response, body) {
+    var parsedResponse = JSON.parse(body),
+        version = parsedResponse.Response.version,
+        language = 'en';
+
+    console.log('Downloading zip for ' + language + '.');
+
+    var zipFilePath = 'manifest/' + language + '/manifest.zip';
+    request.get('https://www.bungie.net' + parsedResponse.Response.mobileWorldContentPaths[language])
+        .pipe(fs.createWriteStream(zipFilePath))
+        .on('close', function () {
+            onManifestDownloaded(zipFilePath, language);
+        });
+}
+
+/**
+ * Handles a manifest being downloaded from Bungie; decompressing and extracting the information.
+ * @param {String} zipFilePath The file path to the compressed manifest.
+ * @param {String} language The language of the manifest file.
+ */
+function onManifestDownloaded(zipFilePath, language) {
+    console.log('Processing zip for ' + language);
+
+    fs.createReadStream(zipFilePath)
+        .pipe(unzip.Parse())
+        .on('entry', function (entry) {
+            var path = 'manifest/' + language + '/' + entry.path;
+            ws = fs.createWriteStream(path)
+            ws.on('finish', function () {
+                if (fs.existsSync(path)) {
+                    exportDefinitionsFromDb(path, language);
+                }
+            });
+            entry.pipe(ws);
+        });
+}
+
+/**
+ * Exports the required definitions from the database file.
+ * @param {Object} dbFile The database file.
+ * @param {String} language The language of the original manifest.
+ */
+function exportDefinitionsFromDb(dbFile, language) {
+    var db = new sqlite3.Database(dbFile);
+
+    db.all('SELECT * FROM DestinyInventoryItemDefinition', function (err, rows) {
+        if (err) throw err;
+
+        var DestinyArmorDefinition = {};
+
+        rows.forEach(function (row) {
+            var item = JSON.parse(row.json);
+
+            // Armor
+            if ((item.itemType === 2) || (item.itemTypeName === 'Mask')) {
+                DestinyArmorDefinition[item.itemHash] = {};
+                DestinyArmorDefinition[item.itemHash].itemName = item.itemName;
+                DestinyArmorDefinition[item.itemHash].itemTypeName = item.itemTypeName;
+                DestinyArmorDefinition[item.itemHash].itemDescription = item.itemDescription;
+                DestinyArmorDefinition[item.itemHash].icon = item.icon;
+                DestinyArmorDefinition[item.itemHash].tierType = item.tierType;
+                DestinyArmorDefinition[item.itemHash].tierTypeName = item.tierTypeName;
+            }
+
+        });
+
+        writeDefinitionFile('client/js/shared/DestinyArmorDefinition.js', 'DestinyArmorDefinition', DestinyArmorDefinition);
+    });
+
+    db.all('SELECT * FROM DestinyStatDefinition', function (err, rows) {
+        if (err) throw err;
+
+        var DestinyStatDefinition = {};
+
+        rows.forEach(function (row) {
+            var item = JSON.parse(row.json);
+            if (item.statHash && item.statName) {
+                DestinyStatDefinition[item.statHash] = {};
+                DestinyStatDefinition[item.statHash].statName = item.statName;
+            }
+        });
+
+        writeDefinitionFile('client/js/shared/DestinyStatDefinition.js', 'DestinyStatDefinition', DestinyStatDefinition);
+    });
+}
